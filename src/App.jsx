bd99d6c39b24a848a0e210910b506507b984c339
Md Rezaul Hossain <str.rezaul@gmail.com>
2025-04-13 18:37:11 +0600

    config redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import React, {use, useEffect, useState} from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-     <h1>React Blog Application</h1>
-    </>
-  )
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}));
+        } else{
+          dispatch(logout());
+        }
+      })
+      .finally(()=>setloading(false));
+  }, []);
+
+  return !loading ? (
+    <div className=''></div>
+  ) : null
 }
 
 export default App
